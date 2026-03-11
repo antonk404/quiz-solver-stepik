ea9c3bd2c6dc39f415bd792d331bd028b7b00cab
Ростислав <rostislav.rostik.sh@gmail.com>
2026-03-11 10:55:13 +0300

    Конфиг для проекта

---
+++ config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
+
+class Settings(BaseSettings):
+    gemini_api_key: str = Field(validation_alias="GEMINI_API_KEY")
+    headless: bool = Field(validation_alias="HEADLESS")
+    retry_attempts: int = Field(validation_alias="RETRY_ATTEMPTS")
+    timeout: int = Field(validation_alias="TIMEOUT") # мс для playwright
+
+    STEPIK_USER_DATA_DIR: str
+
+    @property
+    def stepik_user_data_dir(self) -> Path:
+        return Path(self.STEPIK_USER_DATA_DIR)
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
